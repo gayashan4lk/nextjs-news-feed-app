bc8db11c20653d3bc58d227e872cc75861adc889
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-12-10 01:14:05 +0530

    static site generation - getStaticProps

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,14 @@
 import Link from 'next/link';
 
-export default function News() {
+export default function News({ results }) {
 	return (
 		<>
 			<h1>Top Stories</h1>
+			<ul>
+				{results.map((result) => (
+					<li key={result.uri}>{result.title}</li>
+				))}
+			</ul>
 			<Link href={'/'}>Go to Home</Link>
 		</>
 	);
@@ -12,7 +17,14 @@ export default function News() {
 const API_KEY = 'dzvOipA1Em2L0Ga2yYmpy4cLX3EAkvDZ';
 
 export async function getStaticProps() {
+	const URL = `https://api.nytimes.com/svc/topstories/v2/science.json?api-key=${API_KEY}`;
+	const response = await fetch(URL);
+	const data = await response.json();
+	console.log(data);
+
 	return {
-		props: {},
+		props: {
+			results: data.results,
+		},
 	};
 }
