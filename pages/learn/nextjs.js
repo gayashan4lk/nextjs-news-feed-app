3c86aad8d04ae48ff5720ff3697dee07593fb50e
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-12-09 16:15:58 +0530

    dynamic routing

--- pages/learn/nextjs.js
+++ pages/learn/nextjs.js
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+
 export default function LearnNextJs() {
-	return <h1>Learn NextJs</h1>;
+	return (
+		<>
+			<h1>Learn NextJs</h1>
+			<Link href={'/'}>Go to Home</Link>
+		</>
+	);
 }
